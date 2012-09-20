8bead287f1d56514cbb0e16be22d2683d8dc2ae6
Denis Veselov <progr-m@yandex.ru>
2012-09-21 00:37:49 +0400

    add setup.py and other many important files

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='pyvka',
+    version='0.1',
+    license='ISC',
+    description='Python package for authorize and get'
+                'access_token for vkontakte api.',
+    keywords='vkontakte vk api access_token oauth oauth2',
+    long_description=read('README.rst') + read('CHANGES.rst'),
+    url='https://github.com/saippuakauppias/pyvka',
+    author='Denis Veselov',
+    author_email='progr.mail@gmail.com',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'requests==0.14.0'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
