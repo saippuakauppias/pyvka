c95ce04e60f7c2e36e161ed72e41ca0fb38e847b
Denis Veselov <progr-m@yandex.ru>
2012-09-20 23:53:51 +0400

    add new code for authorize

--- pyvka/auth.py
+++ pyvka/auth.py
@@ -1,4 +1,5 @@
-from urllib import urlencode
+from urlparse import urlparse, parse_qsl
+from urllib import urlencode, unquote_plus
 
 import requests
 
@@ -9,29 +10,71 @@ from .exceptions import AuthError
 class VKAuth(object):
 
     def __init__(self, login, pwd, app_id, scopes, form_parser_class=None):
-        self.login = login
-        self.pwd = pwd
-        self.app_id = app_id
-        self.scopes = scopes
-        self.form_parser_class = form_parser_class or FormParser
+        self._login = login
+        self._pwd = pwd
+        self._app_id = app_id
+        self._scopes = scopes
+        self._form_parser_class = form_parser_class or FormParser
+        self._cookies = {}
+        self.access_token = ''
+        self.used_id = 0
 
-    def get_access_token(self):
+    def _get_access_token(self):
         auth_url = self._generate_auth_url()
         auth_page = requests.get(auth_url)
-        parser = self.form_parser_class()
+        parser = self._form_parser_class()
         parser.feed(auth_page.content)
         parser.close()
         checker = [not parser.form_parsed, parser.url is None,
                    'pass' not in parser.params, 'email' not in parser.params]
-        if  any(checker):
+        if any(checker):
             raise AuthError('Not found form in auth page')
+        parser.params['email'] = self._login
+        parser.params['pass'] = self._pwd
+        auth_sended_page = self._send_request(parser.method, parser.url,
+                                              parser.params)
+        self._cookies = auth_sended_page.cookies
+        if urlparse(auth_sended_page.url).path != '/blank.html':
+            authorized_page = self._get_app_access(auth_sended_page.content)
+            self._parse_authorized_url(authorized_page.url)
+        else:
+            self._parse_authorized_url(auth_sended_page.url)
+
+    def _parse_authorized_url(self, url):
+        url = urlparse(url)
+        query_dict = dict(parse_qsl(url.fragment))
+        if 'error' in query_dict:
+            e = '{0}: {1}'.format(query_dict['error'],
+                                  unquote_plus(query_dict['error_description']))
+            raise AuthError(e)
+        self.access_token = query_dict['access_token']
+        self.used_id = query_dict['user_id']
+
+    def _get_app_access(self, page_data):
+        parser = self._form_parser_class()
+        parser.feed(page_data)
+        parser.close()
+        if not parser.form_parsed or parser.url is None:
+            raise AuthError('Not found form in get application access page')
+        app_access_sended_page = self._send_request(parser.method, parser.url,
+                                                    parser.params)
+        return app_access_sended_page
+
+    def _send_request(self, method, url, data):
+        if method == 'POST':
+            page = requests.post(url, data=data, allow_redirects=True,
+                                 cookies=self._cookies)
+        else:
+            url = '{0}?{1}'.format(url, urlencode(data))
+            page = requests.get(url, cookies=self._cookies)
+        return page
 
     def _generate_auth_url(self):
         query_params = {
-            'client_id': self.app_id,
+            'client_id': self._app_id,
             'redirect_uri': 'http://oauth.vk.com/blank.html',
             'display': 'wap',
-            'scope': ','.join(self.scopes),
+            'scope': ','.join(self._scopes),
             'response_type': 'token'
         }
         query_params = urlencode(query_params)
